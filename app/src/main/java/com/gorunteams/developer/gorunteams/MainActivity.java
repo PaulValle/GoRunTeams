eb83a13f1bda0e41558eef97fada9e522bfa38ef
PaulValle <paul.valle96@gmail.com>
2019-01-03 13:50:49 -0500

    pantallas login

--- app/src/main/java/com/gorunteams/developer/gorunteams/MainActivity.java
+++ app/src/main/java/com/gorunteams/developer/gorunteams/MainActivity.java
@@ -1,7 +1,13 @@
 package com.gorunteams.developer.gorunteams;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.security.Principal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /*
+    Button gosingin = (Button) findViewById(R.id.signin);
+        gosingin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View Object view;
+        view) {
+            Intent itemintent = new Intent(login.this, Principal.class);
+            login.this.startActivity(itemintent);
+        }
+    });
+    */
+
 }
